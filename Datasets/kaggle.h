98e12f8bf98a4c5a4ec400de55de7ae1ca47d585
Yakshith K D <157264298+yakshithkd23@users.noreply.github.com>
2025-03-27 08:16:58 +0530

    Create kaggle.h

---
+++ Datasets/kaggle.h
@@ -0,0 +1,9 @@
+Dataset
+
+We utilize the dataset from Kaggle:https://www.kaggle.com/datasets/sampadab17/network-intrusion-detection 
+  
+  The dataset consists of:
+
+train.csv: Training data for model training.
+
+test.csv: Testing data for model evaluation.
